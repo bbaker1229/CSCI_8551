b2f308a20421acc1220938ac8ae6f8667f5379f8
Andi Gunderson <gunde406@umn.edu>
2021-10-26 19:34:07 -0500

    Add simple random walk for e-puck
    This can be improved later but is sufficent for testing the
    other features we need to design
    
    Signed-off-by: Andi Gunderson <gunde406@umn.edu>

--- controllers/epuck_feature_detection/epuck_feature_detection.py
+++ controllers/epuck_feature_detection/epuck_feature_detection.py
@@ -3,30 +3,44 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+import random
+    
+def random_walk(leftMotor, rightMotor):
+    directions = ["forward", "left", "right"]
+    direction = random.choices(directions, weights = [10, 1, 1], k = 1)
+    if direction[0] == "left":
+        leftMotor.setVelocity(-0.1 * MAX_SPEED)
+        rightMotor.setVelocity(0.1 * MAX_SPEED)
+    if direction[0] == "right":
+        leftMotor.setVelocity(0.1 * MAX_SPEED)
+        rightMotor.setVelocity(-0.1 * MAX_SPEED)
+    if direction[0] == "forward":
+        leftMotor.setVelocity(0.1 * MAX_SPEED)
+        rightMotor.setVelocity(0.1 * MAX_SPEED)
 
+MAX_SPEED = 10
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+    # set up the motor speeds at 10% of the MAX_SPEED.
+leftMotor.setVelocity(0.1 * MAX_SPEED)
+rightMotor.setVelocity(0.1 * MAX_SPEED)
 
+count = 0
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
+    count += 1
+    if count == 30:
+        random_walk(leftMotor, rightMotor)
+        count = 0
     pass
 
 # Enter here exit cleanup code.
