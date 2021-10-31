761ab2ddfc088ea1ff86fc1e3ef277c196d4adbd
Bryan Baker <bbaker1229@gmail.com>
2021-10-31 01:26:26 -0500

    Added movement, estimate, and confidence calc per the paper.

--- controllers/epuck_feature_detection/epuck_feature_detection.py
+++ controllers/epuck_feature_detection/epuck_feature_detection.py
@@ -1,65 +1,130 @@
 """epuck_feature_detection controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+import numpy as np
 import random
 
-def get_color(camera):
-    image = camera.getImageArray()
-    gray = 0
-    for x in range(0,camera.getWidth()):
-        for y in range(0,camera.getHeight()):
-            red   = image[x][y][0]
-            green = image[x][y][1]
-            blue  = image[x][y][2]
-            gray  += (red + green + blue) / 3
+BODYLENGTH = 71
+MAX_SPEED = 1 * BODYLENGTH
+MAX_ROTATION = 0.628  # 1 full rotation in 10s
+COM_RADIUS = 3 * BODYLENGTH
+myid = random.randint(0, 100)  # Need a better method to do this
+print("My ID is: ", myid)
 
-    total_gray = gray/(camera.getWidth()*camera.getHeight())
-    print('gray='+str(total_gray))
-    total_gray
-    
-def random_walk(leftMotor, rightMotor):
-    directions = ["forward", "left", "right"]
-    direction = random.choices(directions, weights = [10, 1, 1], k = 1)
-    if direction[0] == "left":
-        leftMotor.setVelocity(-0.1 * MAX_SPEED)
-        rightMotor.setVelocity(0.1 * MAX_SPEED)
-    if direction[0] == "right":
-        leftMotor.setVelocity(0.1 * MAX_SPEED)
-        rightMotor.setVelocity(-0.1 * MAX_SPEED)
-    if direction[0] == "forward":
-        leftMotor.setVelocity(0.1 * MAX_SPEED)
-        rightMotor.setVelocity(0.1 * MAX_SPEED)
-
-MAX_SPEED = 10
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# Enable camera on the epuck
-camera = robot.getDevice('camera')
-camera.enable(timestep)
+# Enable ground sensor
+gs = robot.getDevice('gs1')
+gs.enable(timestep)
 
+# Enable
+ps = robot.getDevice('ps0')
+ps.enable(timestep)
+
+# Enable motors
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
 leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
-    # set up the motor speeds at 10% of the MAX_SPEED.
-leftMotor.setVelocity(0.1 * MAX_SPEED)
-rightMotor.setVelocity(0.1 * MAX_SPEED)
 
-count = 0
+# Initialize forward movement
+leftMotor.setVelocity(MAX_SPEED)
+rightMotor.setVelocity(MAX_SPEED)
+forward_time = np.random.exponential(14, 1)  # use 240 instead of 14
+forward_flg = True
+rotation_time = 0
+turn_flg = False
+com_period = 0
+com_flg = False
+
+# Initialize time
+start_time = robot.getTime()
+
+# Initialize observation window timer
+window_target_time = start_time + 60
+white_time = 0
+black_time = 0
+time_left = 0
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    count += 1
-    get_color(camera)
-    if count == 30:
-        random_walk(leftMotor, rightMotor)
-        count = 0
+    time_delta = robot.getTime() - start_time
+    if forward_flg:
+        if ps.getValue() > 100:  # If the wall is detected go into turn mode
+            # Set motors to turn
+            leftMotor.setVelocity(MAX_ROTATION)
+            rightMotor.setVelocity(-MAX_ROTATION)
+            # Adjust flags
+            turn_flg = True
+            forward_flg = False
+            # Set the rotation time
+            rotation_time = np.pi / MAX_ROTATION
+            start_time = robot.getTime()
+            time_delta = robot.getTime() - start_time
+            # Pause observation window
+            time_left = window_target_time - robot.getTime()
+        if time_delta >= forward_time:  # If we are in forward mode and we hit the time limit; turn
+            # Adjust flags
+            turn_flg = True
+            forward_flg = False
+            # Set the rotation time
+            start_time = robot.getTime()
+            time_delta = robot.getTime() - start_time
+            rotation = np.random.uniform(-np.pi, np.pi, 1)
+            rotation_time = rotation / MAX_ROTATION
+            # Set motors to turn
+            leftMotor.setVelocity(MAX_ROTATION)
+            rightMotor.setVelocity(-MAX_ROTATION)
+            # Pause observation window
+            time_left = window_target_time - robot.getTime()
+        if robot.getTime() >= window_target_time:  # If we reach the full time window then reset the timers
+            estimate = round(white_time / (white_time + black_time), 3)
+            print(estimate)
+            confidence = max(white_time, black_time) / (white_time + black_time)
+            print(confidence)
+            com_period = confidence * 120
+            window_target_time = robot.getTime() + 60
+            white_time = 0
+            black_time = 0
+            forward_flg = False
+            turn_flg = False
+            com_flg = True
+            leftMotor.setVelocity(0)
+            rightMotor.setVelocity(0)
+        else:
+            if gs.getValue() > 650 and forward_flg:
+                white_time += 1
+            if gs.getValue() < 650 and forward_flg:
+                black_time += 1
+    if com_flg:
+        print("Myid: ", myid, end=',')
+        print("Estimate: ", estimate)
+        if time_delta >= com_period:
+            forward_flg = True
+            turn_flg = False
+            com_flg = False
+            window_target_time = robot.getTime() + 60
+            leftMotor.setVelocity(MAX_SPEED)
+            rightMotor.setVelocity(MAX_SPEED)
+    if turn_flg:
+        if time_delta >= rotation_time:  # If we are in turn mode and we hit the time limit; forward
+            # Adjust flags
+            turn_flg = False
+            forward_flg = True
+            # Set the forward time
+            start_time = robot.getTime()
+            time_delta = robot.getTime() - start_time
+            forward_time = np.random.exponential(14, 1)
+            # Set motors to forward
+            leftMotor.setVelocity(MAX_SPEED)
+            rightMotor.setVelocity(MAX_SPEED)
+            # Reset observation window timer
+            window_target_time = robot.getTime() + time_left
     pass
 
 # Enter here exit cleanup code.
